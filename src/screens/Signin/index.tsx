8449c9300de2b2754462244d7a00182cbdb3cc61
Ana Clara Mendes Bull <anaclaramendesbull@gmail.com>
2022-09-22 15:14:00 -0300

    up

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
+import { Image, ImageBackground, StyleSheet } from 'react-native';
+
 
 import {
-  Container,
+  Container, Header, Title, SubTitle
 } from './styles';
 
-export function Signin(){
-  return (
-    <Container>
+const image = { uri: "https://images.pexels.com/photos/2123773/pexels-photo-2123773.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" };
 
-    </Container>
+export function Signin() {
+  return (
+    <>
+      <ImageBackground
+        source={image} resizeMode="cover" style={styles.image}
+      >
+        <Container>
+
+          <Header>
+            <Title>Boas vindas ao Adestre Amando!</Title>
+            <SubTitle>Faça login ou cadastre-se para uma melhor experiência!</SubTitle>
+          </Header>
+
+        </Container>
+      </ImageBackground>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+    justifyContent: "center"
+  }
+});
